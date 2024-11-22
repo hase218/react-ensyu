c3e54b7af8ab54a5f563f3cb91f5698f07054a2f
hase218 <chju23041@g.nihon-u.ac.jp>
2024-11-22 10:12:54 +0900

    猫に変えた

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { useEffect, useState } from "react"
 
 export default function App() {
-    //randomdogを使ってわんこを表示させてみる
+    //catsを使ってﾇｯｺを表示させてみる
+    //画像の幅を統一させる
     const [img, setImg] = useState();
 
     useEffect(() => {
         (async () => {
             try {
-                const response = await fetch("https://random.dog/woof.json");
+                const response = await fetch("https://api.thecatapi.com/v1/images/search");
                 if (!response.ok) { // ステータスコードが200番台でなければエラーをスロー
                     throw new Error(`HTTPエラー: ${response.status}`);
                 }
                 const data = await response.json();
-                console.log(data.url);
-                setImg(data.url);
+                console.log(data[0].url);
+                setImg(data[0].url);
             } catch (error) {
                 console.error(error.message);
             }
@@ -23,11 +24,11 @@ export default function App() {
     return (
         <>
             <header>
-                <h1>Dogs</h1>
+                <h1>Cats</h1>
             </header>
             <div>
                 <main>
-                    <p>inu</p>
+                    <p>neko</p>
                     <img src={img} alt="randomDogs" />
                 </main>
             </div>
