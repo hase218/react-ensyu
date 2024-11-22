27aa39843a64012a1972497247c461be832d1e5e
hase218 <chju23041@g.nihon-u.ac.jp>
2024-11-22 10:03:23 +0900

    非同期処理でrandomdogs使った

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import { useEffect, useState } from "react"
+
 export default function App() {
+    //randomdogを使ってわんこを表示させてみる
+    const [img, setImg] = useState();
 
-
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await fetch("https://random.dog/woof.json");
+                if (!response.ok) { // ステータスコードが200番台でなければエラーをスロー
+                    throw new Error(`HTTPエラー: ${response.status}`);
+                }
+                const data = await response.json();
+                console.log(data.url);
+                setImg(data.url);
+            } catch (error) {
+                console.error(error.message);
+            }
+        })();
+    }, []);
+    //空の配列を渡した場合は、最初にコンポーネントがレンダリングされた時の 1 回だけ副作用が起こされます
     return (
         <>
             <header>
@@ -9,12 +28,13 @@ export default function App() {
             <div>
                 <main>
                     <p>inu</p>
+                    <img src={img} alt="randomDogs" />
                 </main>
             </div>
             <footer>
                 aaaaa
             </footer>
-            
+
         </>
     )
 }
