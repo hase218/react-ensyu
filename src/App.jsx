b0f2ce1cee716952d3fe03aeea508ab1dcd86404
hase218 <chju23041@g.nihon-u.ac.jp>
2024-11-22 09:31:58 +0900

    scripts変更 css持ってきた 基本的な要素をAppにいれた

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+export default function App() {
+
+
+    return (
+        <>
+            <header>
+                <h1>Dogs</h1>
+            </header>
+            <div>
+                <main>
+                    <p>inu</p>
+                </main>
+            </div>
+            <footer>
+                aaaaa
+            </footer>
+            
+        </>
+    )
+}
