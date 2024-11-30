1d5aa580dc8208b7e06a28c6aa00db7c5a8d8bd1
hase218 <chju23041@g.nihon-u.ac.jp>
2024-11-30 16:03:32 +0900

    スライドしたとき、写真が配列にそれぞれ格納されるようにした useEffectの第二引数に配列名をぶち込んだ

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Box from '@mui/material/Box';
 
 
 export default function App() {
-    const [img, setImg] = useState();
+    const [catImg, setCatImg] = useState();
     const [isDragging, setIsDragging] = useState(false);
     const [startX, setStartX] = useState(0);
     const [distanceX, setDistanceX] = useState(0);
+    const [goodCats, setGoodCats] = useState([]);
+    const [notGoodCats, setNotGoodCats] = useState([]);
 
     useEffect(() => {
         (async () => {
@@ -19,13 +21,14 @@ export default function App() {
                 }
                 const data = await response.json();
                 console.log(data[0]?.url);
-                setImg(data[0]?.url);
+                setCatImg(data[0]?.url);
                 //?.オプショナルチェーン、
             } catch (error) {
                 console.error(error.message);
             }
         })();
-    }, []);
+    }, [goodCats, notGoodCats]);
+    // 第 2 引数には、その副作用が依存する値のリストを配列で渡します。この配列のいずれかの値が、前に副作用を起こした時の値から変わっていたら、再度副作用を起こします。
     //空の配列を渡した場合は、最初にコンポーネントがレンダリングされた時の 1 回だけ副作用が起こされます
 
     // マウスダウンでドラッグ開始
@@ -58,11 +61,14 @@ export default function App() {
 
     // スライド右に反応したときの処理
     const handleSlideRight = () => {
-        alert("画像が右にスライドされました！");
-        // ここで別の処理を追加できます（例えば、画像を変更する等）
+        // alert("画像が右にスライドされました！");
+        setGoodCats(prevImg => [...prevImg, catImg]);
+        console.log("good", goodCats)
     };
     const handleSlideLeft = () => {
-        alert("左にスライドされた")
+        // alert("左にスライドされた")
+        setNotGoodCats(prevImg => [...prevImg, catImg]);
+        console.log("notGood", notGoodCats)
     }
 
     return (
@@ -100,7 +106,7 @@ export default function App() {
 
                             <img
                                 class="cats"
-                                src={img}
+                                src={catImg}
                                 alt="randomCats"
                                 style={{
                                     width: "100%",
