f0ae8c25c738996b63242381a18b835743bbe8c9
hase218 <chju23041@g.nihon-u.ac.jp>
2024-11-30 13:52:29 +0900

    Boxでスタイルをととのえようとしてみた

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react"
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+
 
 export default function App() {
-    //catsを使ってﾇｯｺを表示させてみる
-    //画像の幅を統一させる
     const [img, setImg] = useState();
-    const [clickedSt, isClickedSt] = useState(false);
+    const [isClicked, setIsClicked] = useState(false);
+
 
     useEffect(() => {
         (async () => {
@@ -32,12 +33,55 @@ export default function App() {
             </header>
             <div>
                 <main>
+                    <Box
+                        sx={{
+                            width: "450px",
+                            margin: "0 auto",
+                            border: "2px solid black",
+                            justifyContent: "center",
+                            alignItems: "center", 
+                            textAlign: "center",
+                        }}>
+                        <p>neko</p>
+                        <Box
+                            // conponent = "section"はページの構造や意味を明確にする場合は必要、見た目を整えるだけなら不要
+                            sx={{
+                                width: "400px",
+                                height: "300px",
+                                overflow: "hidden",
+                                border: "2px solid black",
+                                position: "relative",
+                                display: 'flex',
+                                justifyContent: 'center',  // 横方向の中央揃え
+                                alignItems: 'center',      // 縦方向の中央揃え
+                                textAlign: "center",
+                            }}>
+
+                            <img
+                                class="cats"
+                                src={img}
+                                alt="randomCats"
+                                style={{
+                                    width: "100%",
+                                    height: "100%",
+                                    objectFit: "contain",
+                                }
+                                }
+
+                                
+                            />
+
+                        </Box>
+
+                    </Box>
+
+
+
+
+
                     <Button variant="contained">Contained</Button>
-                    <p>neko</p>
-                    {/* サイズ、object-fitで後で調整 */}
-                    <img class="cats" src={img} alt="randomCats" />
                 </main>
-            </div>
+            </div >
             <footer>
                 aaaaa
             </footer>
